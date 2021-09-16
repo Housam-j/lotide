7b7bead983c25e6a134746df1bd55b7800391587
Housam-j <Housam-j@hotmail.com>
2021-09-16 00:40:00 +0000

    done

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+const letterPositions = function(sentence) {
+  const res = {};
+  for (i=0 ; i< sentence.length ; i++) {
+    if (res[sentence[i]]) {
+      res[sentence[i]].push(i);
+      console.log(res[sentence[i]])
+    } else {
+      res[sentence[i]]=[i];
+    }
+  }
+  return res;
+};
+console.log(letterPositions('LighthouseLabs'))
