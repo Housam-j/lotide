a825bb353b463c151edd9f5eb84f593bb17b03f6
Housam-j <Housam-j@hotmail.com>
2021-09-21 01:09:59 +0000

    updated

--- head.js
+++ head.js
@@ -1,12 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.assert(actual === expected,'Assertion failed: '+actual+' !== '+ expected)
-  }  
-};
+const assertEqual = require('../assertEqual');
+
 const head = function(data){
   return data[0];
-}
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+};
