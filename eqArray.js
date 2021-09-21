ad625aa868f58a1f0a6416df73a8450cbab625ae
Housam-j <Housam-j@hotmail.com>
2021-09-21 01:22:12 +0000

    done

--- eqArray.js
+++ eqArray.js
@@ -1,20 +1,15 @@
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function (arr1,arr2){
   if (arr1.length === arr2.length){
     for ( let i = 0 ; i < arr1.length ; i++) {
-      if ( arr1[i] !== arr2[i] &&  arr1[i].type=== arr2[i].type) {
+      if ( arr1[i] !== arr2[i] ) {
         console.log(arr1 === arr2,'Assertion failed: '+arr1+' !== '+ arr2)
-        return false;         
-      } else {
-        console.log(`Assertion Passed: ${arr1} ===  ${arr2}`);
-        return true;
-        } 
-    }
+        return false;
+      }
+    }  console.log( "Assertion Passed "+arr1+'==='+arr2);
   }
 }
 
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports= eqArrays;
