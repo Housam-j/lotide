a04340cccba6ed2e8c8f7de28e5fcf97a2b1a9e8
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-10-27 19:53:52 -0400

    Update countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -11,22 +11,29 @@ const firstNames = [
 ];
 const result1 =  { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
 
-const countOnly=function(firstNames, result1){
-  res={};
-  for (item in result1) {
+const countOnly = function(firstNames, result1) {
+  let res = [];
+  for (let item in result1) {
     if (item === true) {
       if (firstNames.includes(item)) {
-        res[item] +=1
+        res.push(item);
       }
     }
   }
   return res;
-}
-console.log(countOnly(firstNames, result1));                                   
+};
+console.log(countOnly(firstNames, result1));
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.assert(actual === expected,'Assertion failed: ' + actual  + ' !== ' + expected);
+  }
+};
 
 
-
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
