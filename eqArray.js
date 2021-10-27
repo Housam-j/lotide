fe420ab4d34a97d5eb14d7fb4bdaceede72d3320
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-10-26 20:55:58 -0400

    Update eqArray.js

--- eqArray.js
+++ eqArray.js
@@ -1,13 +1,13 @@
 const assertEqual = require('./assertEqual');
 
 const eqArrays = function (arr1,arr2){
-  if (arr1.length === arr2.length){
-    for ( let i = 0 ; i < arr1.length ; i++) {
-      if ( arr1[i] !== arr2[i] ) {
-        console.log(arr1 === arr2,'Assertion failed: '+arr1+' !== '+ arr2)
+  if (arr1.length === arr2.length) {
+    for (let i = 0 ; i < arr1.length ; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(arr1 === arr2,'Assertion failed: ' + arr1 + ' !== ' + arr2)
         return false;
       }
-    }  console.log( "Assertion Passed "+arr1+'==='+arr2);
+    }  console.log( "Assertion Passed " + arr1 + '===' + arr2);
   }
 }
 
