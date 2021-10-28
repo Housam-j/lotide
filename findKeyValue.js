2a513cd8ac53d76f491c8fdb5b363a09dd1e5c62
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-10-27 21:50:43 -0400

    Update findKeyValue.js

--- findKeyValue.js
+++ findKeyValue.js
@@ -16,3 +16,21 @@ console.log(findKeyValue({
   'sixth':'Thursday',
   'seventh':'Friday'
 } ,'Teusday'));
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.assert(actual === expected,'Assertion failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual(findKeyValue({
+  'first':'Satursay',
+  'Second':'Sunday',
+  'third':'Monday',
+  'forth':'Teusday',
+  'fifth':'Wednesday',
+  'sixth':'Thursday',
+  'seventh':'Friday'
+} ,'Sunday'), 'Second');
