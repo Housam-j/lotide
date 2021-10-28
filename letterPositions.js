cbc080bb73ddae2821ecc6dd84f5e986e714bd1b
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-10-27 21:05:20 -0400

    Update letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -19,3 +19,20 @@ const assertEqual = function(actual, expected) {
     console.assert(actual === expected,'Assertion failed: '+actual+' !== '+ expected)
   }  
 };
+
+const  assertArraysEqual = function(arr1,arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i] &&  arr1[i].type === arr2[i].type) {
+        console.log(arr1 === arr2,'Assertion failed');
+        return false;
+      } else {
+        console.log('Assertion Passed');
+        return true;
+      }
+    }
+  }
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2,3]);
