6c709d39d4748870bf2c69c0c52cceb516d1e460
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-10-27 21:37:16 -0400

    Update findKeyValue.js

--- findKeyValue.js
+++ findKeyValue.js
@@ -1,15 +1,18 @@
-const findKeyValue = function(input, val ) {
-  for (let key in input){
-    if (input[key] === val){
+const findKeyValue = function(input, val) {
+  for (let key in input) {
+    if (input[key] === val) {
       return key;
     }
   }
   return false;
 };
+
 console.log(findKeyValue({
   'first':'Satursay',
   'Second':'Sunday',
   'third':'Monday',
-  'forth':'Thursday',
-  'six':  'Friday'
-    } ,'Sunday'))    
+  'forth':'Teusday',
+  'fifth':'Wednesday',
+  'sixth':'Thursday',
+  'seventh':'Friday'
+} ,'Teusday'));
