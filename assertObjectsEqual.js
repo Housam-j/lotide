28ce8df3b148e1ca39eebc054632d2e54f87ffdc
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-10-27 22:31:05 -0400

    Update assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -5,11 +5,17 @@ const assertEqual = function(actual, expected) {
     console.assert(actual === expected,'Assertion failed: '+actual+' !== '+ expected)
   }  
 };  
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+
+
 const eqObjects = function(object1, object2) {
-  assertEqual(object1, object2)
+  for (let item in object1) {
+    assertEqual(object1[item], object2[item]);
+    if (false ) {
+      return false
+    }
+  }
 };
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba);
