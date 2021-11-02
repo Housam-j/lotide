9c965ed79944ff8d5d9f09eefb00e185fedc9655
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-11-01 20:53:24 -0400

    Update middle.js

--- middle.js
+++ middle.js
@@ -2,17 +2,14 @@ const middle = function(arr) {
   let l =  arr.length / 2;
   let elem = [];
   if (arr.length === 1) {
-    console.log(elem);
+    return elem;
   } else if (arr.length %   2 !== 0) {
     elem.push(arr[Math.ceil(l) - 1 ]);
-    console.log(elem);
+    return elem;
   } else if (arr.length % 2 === 0) {
-    // console.log (l);
     elem.push((arr)[l - 1],(arr[l]));
-    console.log(elem);
+    return elem;
   }
-  // console.log(elem);
 };
-
 
 module.exports = middle;
