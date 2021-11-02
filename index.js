b107f28386361d7800aed41cfe40695710e67e3c
Housam-j <88250801+Housam-j@users.noreply.github.com>
2021-11-01 21:34:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const countOnly   = require('./countOnly');
+const countLetters   = require('./countLetters');
+const assertEqual   = require('./assertEqual');
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertObjectsEqual   = require('./assertObjectsEqual');
+
+countLetters
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
+head,
+tail,
+middle,
+assertEqual,
+assertArraysEqual,
+assertObjectsEqual,
+countLetters,
+countOnly,
 };
